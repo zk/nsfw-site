eb50c8f3da07b892751f40c6f94234a1d148a055
zk <zachary.kim@gmail.com>
2013-07-30 01:51:57 -0700

    Use load-routes in entry

--- src/clj/nsfw_site/entry.clj
+++ src/clj/nsfw_site/entry.clj
@@ -1,16 +1,10 @@
 (ns nsfw-site.entry
-  (:require [nsfw.app :as app]))
+  (:require [nsfw.app :as app]
+            [nsfw-site.intro :as intro]))
 
 (defonce session (app/session-store :encrypted-cookie))
 
 (def app
-  (app/clojurescript
-   :entry 'nsfw-site.app/entry
-   :session-store session
-   :css [:nsfw :app]
-   :data (fn [r]
-           ;; Key / val pairs are embedded into the page as js vars
-           {:unique-value (str (gensym))})
-   :api (app/route
-         ;; Routes mounted at /api
-         ["name"] (fn [r] {:body (pr-str (rand))}))))
+  (app/route-default
+   {}
+   (app/load-routes '[nsfw-site.intro nsfw-site.demos.todo])))
