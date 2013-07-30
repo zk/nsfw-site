5623621738ac4ef55c5dd2f872d9eb7fb4ff2c37
zk <zachary.kim@gmail.com>
2013-07-30 00:36:18 -0700

    Added debug exceptions to default app handler

--- src/clj/nsfw_site/run.clj
+++ src/clj/nsfw_site/run.clj
@@ -1,6 +1,7 @@
 (ns nsfw-site.run
   (:require [nsfw.server :as server]
             [nsfw.env :as env]
+            [nsfw.app :as app]
             [ring.middleware.reload-modified :as reload]
             [nsfw-site.entry :as entry]
             [clojure.tools.nrepl.server :as repl]))
@@ -10,7 +11,8 @@
 
 (def root-entry
   (-> #'entry/app
-      (reload/wrap-reload-modified ["src/clj"])))
+      (reload/wrap-reload-modified ["src/clj"])
+      (app/debug-exceptions true)))
 
 (defn -main [& args]
   (start-repl (env/int :repl-port 7888))
