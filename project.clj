af49563ab33c3532c185c14c356f45c1a29e5118
zk <zachary.kim@gmail.com>
2013-08-09 00:45:30 -0700

    Move cljsbuild to plugins

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "NSFW overview / docs / demo"
   :min-lein-version "2"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [nsfw "0.6.8"]
-                 [lein-cljsbuild "0.3.2"]]
+                 [nsfw "0.6.8"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild {:builds
               {:dev  {:source-paths ["src/cljs"]
                       :compiler {:output-to "resources/public/js/app.js"
