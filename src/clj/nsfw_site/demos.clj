796e317cd17ee082beb276b2bf9324dcc34f8e40
zk <zachary.kim@gmail.com>
2013-08-12 01:57:39 -0700

    Demo section refactor WIP

--- src/clj/nsfw_site/demos.clj
+++ src/clj/nsfw_site/demos.clj
@@ -1,20 +1,50 @@
 (ns nsfw-site.demos
   (:require [nsfw]))
 
-(nsfw/defcomp demo-nav [{:keys [tab]} body]
+
+
+
+(defn make-tab [{:keys [name target-tab selected-tab href on-page-nav]}]
+  (let [active? (= target-tab selected-tab)]
+    [:li {:class (when active? "active")}
+     [:a {:href href} name]
+     (when (and active? on-page-nav)
+       [:ul.on-page-nav
+        (map (fn [{:keys [href text]}]
+               [:li [:a {:href href} text]])
+             on-page-nav)])]))
+
+(nsfw/defcomp demo-nav [{:keys [tab on-page-nav]} body]
   [:ul.nav.demo-nav
    {:data-offset-top 200
     :data-spy "affix"}
-   [:li {:data-tab :autoload
-         :class (when (= :autoload tab) "active")}
-    [:a {:href "/examples/autoload"} "Autoload"]]
-   [:li {:data-tab :the-list
-         :class (when (= :the-list tab) "active")}
-    [:a {:href "/examples/the-list"} "The List"]]
-   [:li.the-list-redux {:class (when (= :the-list-redux tab) "active")}
-    [:a {:href "/examples/the-list-redux"} "The List (Redux)"]]])
+   (map make-tab
+        [{:name "Autoload"
+          :target-tab :autoload
+          :selected-tab tab
+          :href "/examples/autoload"
+          :on-page-nav on-page-nav}
+         {:name "The List"
+          :target-tab :the-list
+          :selected-tab tab
+          :href "/examples/the-list"
+          :on-page-nav on-page-nav}
+         {:name "The List (Redux)"
+          :target-tab :the-list-redux
+          :selected-tab tab
+          :href "/examples/the-list-redux"
+          :on-page-nav on-page-nav}])
 
-(nsfw/defcomp demo [{:keys [tab js-entry]} body]
+   #_[:li.the-list-redux {:class (when (= :the-list-redux tab) "active")}
+    [:a {:href "/examples/the-list-redux"} "The List (Redux)"]
+    (when (and (= :the-list-redux tab)
+               on-page-nav)
+      [:ul.on-page-nav
+       (map (fn [{:keys [href text]}]
+              [:li [:a {:href href} text]])
+            on-page-nav)])]])
+
+(nsfw/defcomp demo [{:keys [tab js-entry nav]} body]
   [:page-body
    {:class "page-demo"
     :active-tab :demos
@@ -32,6 +62,38 @@
      [:div.col-lg-9.demo-content
       body]]]])
 
+(def nav {"autoload"
+          {:name "Autoload"
+           :md "src/md/autoload.md"
+           :subnav [["#intro"       "Intro"]
+                    ["#background"  "Background"]]}
+
+          "the-list"
+          {:name "The List"
+           :md "src/md/the-list.md"
+           :subnav [["#intro"       "Intro"]]
+           :js-entry 'nsfw-site.demos.thelist.main}
+
+          "the-list-redux"
+          {:name "The List (Redux)"
+           :md "src/md/the-list-redux.md"
+           :js-entry 'nsfw-site.demos.redux.main}})
+
+(defn render-example-page [{:keys [name md subnav] :as page}]
+  (nsfw/render
+   [:default-head]
+   [:demo
+
+    [:markdown {:src md}]]))
+
+(nsfw/defroute "/examples/:example"
+  examples [{:keys [route-params] :as r}]
+  (let [example (:example route-params)
+        page-data (get nav example)]
+    (when page-data
+      (-> page-data
+          render-example-page))))
+
 (nsfw/defroute "/examples/the-list"
   the-list [r]
   (nsfw/render
@@ -39,6 +101,11 @@
    [:demo
     {:tab :the-list
      :js-entry "nsfw_site.demos.thelist.main()"}
+
+    [:page-nav-item {:href "#intro"} "Intro"]
+    [:page-nav-item {:href "#data-binding"} "Data Binding"]
+    [:page-nav-item {:href "#all-together"} "All Together"]
+
     [:div#the-list
      [:markdown {:src "src/md/the-list.md"}]]]))
 
@@ -48,7 +115,11 @@
    [:default-head]
    [:demo
     {:tab :the-list-redux
-     :js-entry "nsfw_site.demos.redux.main()"}
+     :js-entry "nsfw_site.demos.redux.main()"
+     :on-page-nav [{:href "#intro"
+                    :text "Intro"}
+                   {:href "#starting-small"
+                    :text "Starting Small"}]}
     [:div#the-list
      [:markdown {:src "src/md/the-list-redux.md"}]]]))
 
@@ -68,6 +139,10 @@
    [:default-head]
    [:demo
     {:tab :autoload
-     :js-entry 'nsfw-site.app.entry}
+     :js-entry 'nsfw-site.app.entry
+     :on-page-nav [{:href "#intro"
+                    :text "Intro"}
+                   {:href "#under-the-hood"
+                    :text "Background"}]}
     [:div#autoload
      [:markdown {:src "src/md/autoload.md"}]]]))
---
+++ src/clj/nsfw_site/docs.clj
@@ -0,0 +1,19 @@
+(ns nsfw-site.docs
+  (:require [nsfw]))
+
+(nsfw/defroute "/docs"
+  docs [r]
+  (nsfw/render
+   [:default-head]
+   [:page-body
+    [:div.container
+     [:div.row
+      [:div.col-lg-12
+       [:div.page-lead
+        [:h1 "Docs"]
+        [:p.lead "Take a look at our tasty docs."]]]]
+     [:div.row
+      [:div.col-lg-3
+       [:sub-nav
+        [:nav-item {:href "#"}
+         "Hello World"]]]]]]))
